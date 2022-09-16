2e4e23b34d77ffa15eba8270ef00df46524e712b
zwelethu <zwelethu.mus@gmail.com>
2022-09-16 08:18:11 -0700

    check if paramter

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper - check if parameter is an uppercaase character.
- * @C: input character.
- * Return: 1 if is an uppercase character. 0 in other case.
+ * _isupper - checks if parameter is an uppercase character.
+ * @c: input character.
+ * Return: 1 if is an uppercase character, 0 in other case.
  */
 int _isupper(int c)
 {
